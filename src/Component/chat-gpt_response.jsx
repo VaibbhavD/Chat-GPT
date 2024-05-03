7d59d77b4b37fb3aa3c9379faf6d1170e4974095
VaibhavDhamanage <vaibhavdhamanage>
2024-05-03 17:16:43 +0530

    make chat gpt response component

---
+++ src/Component/chat-gpt_response.jsx
@@ -0,0 +1,21 @@
+const Chatgpt_res = () => {
+  return (
+    <div class="flex w-full mt-2 space-x-3 max-w-xl m-2">
+      <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300">
+        <img
+          src="https://tse4.mm.bing.net/th?id=OIP.n6aTpKW4pOfH1J91PU5yhwAAAA&pid=Api&P=0&h=180"
+          alt="image"
+        />
+      </div>
+      <div>
+        <div class="bg-gray-300 p-3 rounded-r-lg rounded-bl-lg">
+          <p class="text-sm">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </p>
+        </div>
+        <span class="text-xs text-gray-500 leading-none">2 min ago</span>
+      </div>
+    </div>
+  );
+};
+export default Chatgpt_res;
