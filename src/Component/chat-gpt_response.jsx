5d4a3e928d5b9cae9c838a87c2bee5be86cbab19
VaibhavDhamanage <vaibhavdhamanage>
2024-05-03 18:50:19 +0530

    style update

--- src/Component/chat-gpt_response.jsx
+++ src/Component/chat-gpt_response.jsx
@@ -1,19 +1,21 @@
 const Chatgpt_res = () => {
   return (
-    <div class="flex w-full mt-2 space-x-3 max-w-xl m-2">
-      <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300">
+    <div class="flex w-full mt-2 space-x-3 max-w-xl m-4 font-semibold mb-4 ">
+      <div class="flex-shrink-0 h-10 w-10 rounded-full flex justify-center items-center">
         <img
           src="https://tse4.mm.bing.net/th?id=OIP.n6aTpKW4pOfH1J91PU5yhwAAAA&pid=Api&P=0&h=180"
           alt="image"
+          className="w-5"
         />
       </div>
       <div>
-        <div class="bg-gray-300 p-3 rounded-r-lg rounded-bl-lg">
+        <span class="font-bold ">ChatGPT</span>
+        <div class=" p-3 rounded-r-lg rounded-bl-lg">
           <p class="text-sm">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit.
           </p>
         </div>
-        <span class="text-xs text-gray-500 leading-none">2 min ago</span>
+        {/* <span class="text-xs text-gray-500 leading-none">2 min ago</span> */}
       </div>
     </div>
   );
