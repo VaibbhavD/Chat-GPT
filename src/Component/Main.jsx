e757dd28e375b04c0c46a54e39e2c9e06d6fcdbd
VaibhavDhamanage <vaibhavdhamanage>
2024-05-04 15:44:05 +0530

    create input data object and add into messages array

--- src/Component/Main.jsx
+++ src/Component/Main.jsx
@@ -1,33 +1,56 @@
+import React, { useContext, useState } from "react";
 import UserInput from "./User_input";
 import Chatgpt_res from "./chat-gpt_response";
-import { useState } from "react";
+import UpArrow from "../images/up-arrow.png";
+import Context from "../ContextStore/Store";
 import "../App.css";
 
 const Main = () => {
-  const [Userinput, setUserInput] = useState("");
+  const [userInput, setUserInput] = useState("");
+  const context = useContext(Context);
 
-  const SubmitHandle = () => {
-    <UserInput userin={UserInput} />;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Do something with the user input, like sending it to a chatbot
+    const data = {
+      type: "user",
+      data: userInput,
+      time: new Date().toLocaleTimeString(),
+    };
+    context.AddMesages(data);
+    // Clear input field after submission if needed
+    setUserInput("");
   };
 
   return (
-    <body class="flex flex-col items-center justify-center p-5 max-h-screen h-screen text-gray-800 lg:ml-80">
-      <div class="flex flex-col flex-grow w-full max-w-2xl min-h- overflow-auto scrollbar-hide rounded-lg ">
-        {/* <UserInput user={UserInput} /> */}
+    <div className="flex flex-col items-center justify-center p-5 max-h-screen h-screen text-gray-800 lg:ml-80">
+      <div className="flex flex-col flex-grow w-full max-w-2xl min-h-0 overflow-auto scrollbar-hide rounded-lg">
+        {context.Messages ? (
+          context.Messages.map((mess) => <Chatgpt_res user={mess} />)
+        ) : (
+          <h1>Vaibhav</h1>
+        )}
+
+        {/* Render chat responses here if applicable */}
       </div>
-      <div class=" p-4 w-full max-w-2xl flex">
+      <form
+        onSubmit={handleSubmit}
+        className="w-full max-w-2xl flex rounded-xl border-2 p-2"
+      >
         <input
-          class="flex items-center h-10 w-full min-w-2xl rounded-xl p-6 text-sm border-2"
+          className="flex items-center h-4 w-full min-w-2xl rounded-xl p-4 text-sm outline-none font-bolder"
           type="text"
           placeholder="Type your message…"
-          name="userin"
+          value={userInput}
           onChange={(e) => setUserInput(e.target.value)}
+          required
         />
-        <button type="button" onClick={SubmitHandle}>
-          ^|
+        <button type="submit" className="text-black">
+          <img src={UpArrow} className="w-5 m-2" alt="Send" />
         </button>
-      </div>
-    </body>
+      </form>
+    </div>
   );
 };
+
 export default Main;
