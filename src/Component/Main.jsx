deb30ead8bd2a0feebcb079179128ef994c554d0
Vaibhav D <vaibhavdhamanage12@gmail.com>
2024-06-16 16:49:18 +0530

    auto scroll when add messages and loader updates

--- src/Component/Main.jsx
+++ src/Component/Main.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import UserInput from "./User_input";
 import Chatgpt_res from "./chat-gpt_response";
 import UpArrow from "../images/up-arrow.png";
@@ -9,8 +9,10 @@ import Home from "./Home/Home";
 const Main = () => {
   const [userInput, setUserInput] = useState("");
   const context = useContext(Context);
+  const contentRef = useRef(null);
 
   const handleSubmit = async (e) => {
+    context.Setloader(true);
     e.preventDefault();
     // Do something with the user input, like sending it to a chatbot
     const data = {
@@ -23,16 +25,27 @@ const Main = () => {
     // context.Setloader();
     // console.log(context.loader);
   };
+  const scrollToBottom = () => {
+    const contentDiv = contentRef.current;
+    contentDiv.scrollTop = contentDiv.scrollHeight;
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [context.Messages, context.loader]);
 
   return (
     <div className="flex flex-col items-center justify-center max-h-screen h-screen text-gray-800 lg:ml-72 lg:pl-10 pb-5 bg-black">
       <nav className="w-full h-4 py-7 flex justify-between items-center font-semibold text-lg text-white ">
         <select className="border-none font-bolder text-md p-2 rounded-md bg-transparent ">
-          <option>Gemini</option>
-          <option>GeminiAi 4.0</option>
+          <option className="bg-black">GeminiAi 4.0</option>
+          <option className="bg-black">Gemini</option>
         </select>
       </nav>
-      <div className="flex flex-col flex-grow w-full max-w-2xl min-h-0 overflow-auto scrollbar-hide rounded-lg p-4">
+      <div
+        className="flex flex-col flex-grow w-full max-w-2xl min-h-0 overflow-auto scrollbar-hide rounded-lg p-4"
+        ref={contentRef}
+      >
         {context.Messages.length > 0 ? (
           context.Messages.map((mess) => <UserInput user={mess} />)
         ) : (
@@ -53,7 +66,11 @@ const Main = () => {
           onChange={(e) => setUserInput(e.target.value)}
           required
         />
-        <button type="submit" className="bg-white rounded-full">
+        <button
+          type="submit"
+          className="bg-white rounded-full"
+          disabled={context.loader === true ? true : false}
+        >
           <img src={UpArrow} className="w-5 m-2 bg-white" alt="Send" />
         </button>
       </form>
