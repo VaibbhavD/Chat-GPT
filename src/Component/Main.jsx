681a59ff341e83a6d2b3672bdac6dfe5855ffee2
Vaibhav D <vaibhavdhamanage12@gmail.com>
2024-06-16 10:39:50 +0530

    rename and adjuct margine and padding

--- src/Component/Main.jsx
+++ src/Component/Main.jsx
@@ -4,7 +4,7 @@ import Chatgpt_res from "./chat-gpt_response";
 import UpArrow from "../images/up-arrow.png";
 import Context from "../ContextStore/Store";
 import "../App.css";
-import { run } from "../ContextStore/ChatGPTapi";
+import Home from "./Home/Home";
 
 const Main = () => {
   const [userInput, setUserInput] = useState("");
@@ -25,37 +25,36 @@ const Main = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center max-h-screen h-screen text-gray-800 lg:ml-80 pb-5">
-      <nav className="w-full h-4 py-7 flex justify-between items-center font-semibold text-lg text-stone-500 ">
-        <select className="border-none font-bolder text-md p-2 rounded-md hover:bg-stone-100">
-          <option>ChatGPT</option>
-          <option>ChatGPT 4.0</option>
+    <div className="flex flex-col items-center justify-center max-h-screen h-screen text-gray-800 lg:ml-72 lg:pl-10 pb-5 bg-black">
+      <nav className="w-full h-4 py-7 flex justify-between items-center font-semibold text-lg text-white ">
+        <select className="border-none font-bolder text-md p-2 rounded-md bg-transparent ">
+          <option>Gemini</option>
+          <option>GeminiAi 4.0</option>
         </select>
-        <div>Dark Mode</div>
       </nav>
       <div className="flex flex-col flex-grow w-full max-w-2xl min-h-0 overflow-auto scrollbar-hide rounded-lg p-4">
-        {context.Messages ? (
+        {context.Messages.length > 0 ? (
           context.Messages.map((mess) => <UserInput user={mess} />)
         ) : (
-          <h1>Vaibhav</h1>
+          <Home />
         )}
 
         {/* Render chat responses here if applicable */}
       </div>
       <form
         onSubmit={handleSubmit}
-        className="w-full max-w-2xl flex rounded-xl border-2 p-2"
+        className="w-full max-w-2xl flex rounded-xl border-2 p-2 bg-[#1E1F20]"
       >
         <input
-          className="flex items-center h-4 w-full min-w-2xl rounded-xl p-4 text-md outline-none font-bolder"
+          className="flex items-center text-white h-4 w-full min-w-2xl rounded-xl p-4 text-md outline-none font-bolder bg-[#1E1F20]"
           type="text"
           placeholder="Type your message…"
           value={userInput}
           onChange={(e) => setUserInput(e.target.value)}
           required
         />
-        <button type="submit" className="text-black">
-          <img src={UpArrow} className="w-5 m-2" alt="Send" />
+        <button type="submit" className="bg-white rounded-full">
+          <img src={UpArrow} className="w-5 m-2 bg-white" alt="Send" />
         </button>
       </form>
     </div>
