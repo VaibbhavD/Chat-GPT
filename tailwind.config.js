5f16a132601b55de015caf144a67b642f3a206a5
Vaibhav D <vaibhavdhamanage12@gmail.com>
2024-06-16 16:48:29 +0530

    add animation for gemini icon logo rotate

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,11 @@ export default {
       colors: {
         "custom-pink-blue": "#D4A5FF", // Define a custom color
       },
+      animation: {
+        "spin-slow": "spin 3s linear infinite",
+        "spin-medium": "spin 1.5s linear infinite",
+        "spin-fast": "spin 0.75s linear infinite",
+      },
     },
   },
   plugins: ["tailwind-scrollbar-hide"],
