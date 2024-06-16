a044f9278699660275740fb0428eba3a08d6d3c9
Vaibhav D <vaibhavdhamanage12@gmail.com>
2024-06-16 10:39:33 +0530

    add gemini icon

--- src/Component/chat-gpt_response.jsx
+++ src/Component/chat-gpt_response.jsx
@@ -25,22 +25,43 @@ const Chatgpt_res = (props) => {
   return (
     <>
       <div className="flex items-start gap-2.5 pb-5">
-        <img
-          className="w-8 h-8 rounded-full"
-          src="https://tse4.mm.bing.net/th?id=OIP.n6aTpKW4pOfH1J91PU5yhwAAAA&pid=Api&P=0&h=180"
-          alt="Bot image"
-        />
+        <svg
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 16 16"
+          width={30}
+          // className="rotate-6"
+        >
+          <path
+            d="M16 8.016A8.522 8.522 0 008.016 16h-.032A8.521 8.521 0 000 8.016v-.032A8.521 8.521 0 007.984 0h.032A8.522 8.522 0 0016 7.984v.032z"
+            fill="url(#prefix__paint0_radial_980_20147)"
+          />
+          <defs>
+            <radialGradient
+              id="prefix__paint0_radial_980_20147"
+              cx="0"
+              cy="0"
+              r="1"
+              gradientUnits="userSpaceOnUse"
+              gradientTransform="matrix(16.1326 5.4553 -43.70045 129.2322 1.588 6.503)"
+            >
+              <stop offset=".067" stop-color="#9168C0" />
+              <stop offset=".343" stop-color="#5684D1" />
+              <stop offset=".672" stop-color="#1BA1E3" />
+            </radialGradient>
+          </defs>
+        </svg>
         <div className="flex flex-col gap-1 w-full max-w-[600px]">
           <div className="flex items-center space-x-2 rtl:space-x-reverse">
-            <span className="text-sm font-semibold text-gray-900 dark:text-white">
-              ChatGPT
+            <span className="text-sm font-semibold  text-blue-500">
+              Gemini <span className="text-pink-300">AI</span>
             </span>
             <span className="text-xs font-normal text-gray-500 dark:text-gray-400">
               {props.user.time}
             </span>
           </div>
           <div className="flex flex-col leading-1.5 p-4 border-gray-200  rounded-e-xl rounded-es-xl dark:bg-gray-700">
-            <p className="text-sm font-normal text-gray-900 dark:text-white">
+            <p className="text-sm font-normal text-white">
               {displayedText && displayedText}
               {!displayedText && <Loader />}
             </p>
