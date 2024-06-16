326ee7e326fbb4fd7760ec1d870d7a947081f61d
Vaibhav D <vaibhavdhamanage12@gmail.com>
2024-06-16 17:29:00 +0530

    icon replace images

--- src/Component/chat-gpt_response.jsx
+++ src/Component/chat-gpt_response.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
 import Loader from "./loader/Loader";
 import Context from "../ContextStore/Store";
+import gemini from "../images/gemini.png";
 
 const Chatgpt_res = (props) => {
   const [displayedText, setDisplayedText] = useState("");
@@ -28,32 +29,11 @@ const Chatgpt_res = (props) => {
   return (
     <>
       <div className="flex items-start gap-2.5 pb-5">
-        <svg
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 16 16"
+        <img
+          src={gemini}
           width={30}
           className={`${!displayedText && "animate-spin-medium"}`}
-        >
-          <path
-            d="M16 8.016A8.522 8.522 0 008.016 16h-.032A8.521 8.521 0 000 8.016v-.032A8.521 8.521 0 007.984 0h.032A8.522 8.522 0 0016 7.984v.032z"
-            fill="url(#prefix__paint0_radial_980_20147)"
-          />
-          <defs>
-            <radialGradient
-              id="prefix__paint0_radial_980_20147"
-              cx="0"
-              cy="0"
-              r="1"
-              gradientUnits="userSpaceOnUse"
-              gradientTransform="matrix(16.1326 5.4553 -43.70045 129.2322 1.588 6.503)"
-            >
-              <stop offset=".067" stop-color="#9168C0" />
-              <stop offset=".343" stop-color="#5684D1" />
-              <stop offset=".672" stop-color="#1BA1E3" />
-            </radialGradient>
-          </defs>
-        </svg>
+        />
 
         <div className="flex flex-col gap-1 w-full max-w-[600px]">
           <div className="flex items-center space-x-2 rtl:space-x-reverse">
